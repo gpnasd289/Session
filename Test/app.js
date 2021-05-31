d7206f1066030d19bb3850a5dbb55ef1c8c01623
gpnasd289 <68735057+gpnasd289@users.noreply.github.com>
2021-05-31 11:43:52 +0700

    Update app.js

--- Test/app.js
+++ Test/app.js
@@ -100,3 +100,16 @@
 
 // 5.Viết chương trình cho phép người dùng nhập vào ngày tháng năm
 // Kiểm tra xem ngày tháng năm đó có hợp lệ hay không
+let a =  prompt("Enter date")
+function checkDate(strDate) {
+    let comp = strDate.split('/')
+    let d = parseInt(comp[0], 10)
+    let m = parseInt(comp[1], 10)
+    let y = parseInt(comp[2], 10)
+    let date = new Date(y,m-1,d);
+    if (date.getFullYear() == y && date.getMonth() + 1 == m && date.getDate() == d) {
+        return true
+    }
+    return false
+}
+console.log(checkDate(a));
